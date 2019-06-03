23c5e3cb4c3c98ebe579f8c5dc14181bc1d57291
assane savadogo <assane.savadogo81@gmail.com>
2019-06-03 04:13:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-                  patriarch:
-                  matriarch:
-                  hiro:
-                  hero_friends:
+                  patriarch: {},
+                  matriarch: {},
+                  hiro: {},
+                  hero_friends: []
       }, 
     :capulet => {
-      #your key/value pairs here!
+                  patriarch: {},
+                  matriarch:{},
+                  heroine: {},
+                  heroine_friends: []
       }}
 
   
