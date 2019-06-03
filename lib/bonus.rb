ae22c0cfcde15d119da29ce789797941e0a6bae0
assane savadogo <assane.savadogo81@gmail.com>
2019-06-03 04:44:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,7 +19,7 @@ def bonus
       ]
    }
   }
-epic_tragedy[:patriarch][:hero][status:]
+epic_tragedy[:patriarch][:hero][status: "dead"]
 epic_tragedy[:capulet][:heroine][status: "dead"]
 puts epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
